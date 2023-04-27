bcbc6c7b7ff5298a3a78154d138ba90130a0d721
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2023-04-27 16:49:40 +0200

    Update script.js

--- pngpeek/script.js
+++ pngpeek/script.js
@@ -1,15 +1,34 @@
-const htmlInput = document.getElementById('html-input');
-const detectButton = document.getElementById('detect-button');
-const pngImages = document.getElementById('png-images');
-const pngRegex = /<img[^>]*src="([^"]*\.png)"[^>]*>/g;
-function detectPNGs() {
-	pngImages.innerHTML = '';
-	const htmlCode = htmlInput.value;
-	let match;
-	while ((match = pngRegex.exec(htmlCode)) !== null) {
-		const img = document.createElement('img');
-		img.src = match[1];
-		pngImages.appendChild(img);
-	}
-}
-detectButton.addEventListener('click', detectPNGs);
+const detectPngs = async () => {
+  const url = document.getElementById("url-input").value.trim();
+  if (!url) {
+    alert("Please enter a valid website URL.");
+    return;
+  }
+  
+  const response = await fetch(url);
+  const html = await response.text();
+  
+  const pngRegex = /<img[^>]+src="?((.+?)\.png)"?[^>]*>/gi;
+  const pngMatches = html.match(pngRegex);
+  
+  if (!pngMatches) {
+    alert("No PNG images found.");
+    return;
+  }
+  
+  const pngImagesDiv = document.getElementById("png-images");
+  pngImagesDiv.innerHTML = "";
+  
+  for (let i = 0; i < pngMatches.length; i++) {
+    const pngMatch = pngMatches[i];
+    const pngSrcRegex = /src="([^"]+)"/i;
+    const pngSrcMatch = pngMatch.match(pngSrcRegex);
+    const pngSrc = pngSrcMatch ? pngSrcMatch[1] : "";
+    const pngImg = document.createElement("img");
+    pngImg.src = pngSrc;
+    pngImagesDiv.appendChild(pngImg);
+  }
+};
+
+const detectButton = document.getElementById("detect-button");
+detectButton.addEventListener("click", detectPngs);

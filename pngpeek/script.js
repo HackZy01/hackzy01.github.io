15ee714b13f3590b0323b235ffe2d43fb48db660
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2023-04-27 21:39:14 +0200

    Update script.js

--- pngpeek/script.js
+++ pngpeek/script.js
@@ -1,30 +1,15 @@
-// Get the necessary HTML elements
-const htmlInput = document.getElementById('html-input');
-const detectButton = document.getElementById('detect-button');
-const pngImages = document.getElementById('png-images');
+const form = document.getElementById('form');
+const textarea = document.getElementById('textarea');
+const output = document.getElementById('output');
 
-// Define the regular expression to detect PNG files
-const pngRegex = /<img[^>]*src="([^"]*\.png)"[^>]*>/g;
-
-// Define the function to detect PNGs and display them
-function detectPNGs() {
-	// Clear any existing images
-	pngImages.innerHTML = '';
-
-	// Get the HTML code from the input
-	const htmlCode = htmlInput.value;
-
-	// Find all matches of the PNG regular expression in the HTML code
-	let match;
-	while ((match = pngRegex.exec(htmlCode)) !== null) {
-		// Create a new image element and set its source to the matched PNG URL
-		const img = document.createElement('img');
-		img.src = match[1];
-
-		// Append the image element to the PNG images container
-		pngImages.appendChild(img);
-	}
-}
-
-// Add an event listener to the detect button to call the detectPNGs function
-detectButton.addEventListener('click', detectPNGs);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const html = textarea.value;
+  const regex = /<img[^>]+src="?((.+?)\.(png|jpg|jpeg))"?[^>]*>/g;
+  let match;
+  let images = '';
+  while ((match = regex.exec(html)) !== null) {
+    images += `<img src="${match[1]}">`;
+  }
+  output.innerHTML = images;
+});

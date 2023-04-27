ba7a056920ee7b94d4c3af2a3d88ffafac025cdb
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2023-04-27 16:54:26 +0200

    Update script.js

--- pngpeek/script.js
+++ pngpeek/script.js
@@ -1,34 +1,15 @@
-const detectPngs = async () => {
-  const url = document.getElementById("url-input").value.trim();
-  if (!url) {
-    alert("Please enter a valid website URL.");
-    return;
-  }
-  
-  const response = await fetch(url);
-  const html = await response.text();
-  
-  const pngRegex = /<img[^>]+src="?((.+?)\.png)"?[^>]*>/gi;
-  const pngMatches = html.match(pngRegex);
-  
-  if (!pngMatches) {
-    alert("No PNG images found.");
-    return;
-  }
-  
-  const pngImagesDiv = document.getElementById("png-images");
-  pngImagesDiv.innerHTML = "";
-  
-  for (let i = 0; i < pngMatches.length; i++) {
-    const pngMatch = pngMatches[i];
-    const pngSrcRegex = /src="([^"]+)"/i;
-    const pngSrcMatch = pngMatch.match(pngSrcRegex);
-    const pngSrc = pngSrcMatch ? pngSrcMatch[1] : "";
-    const pngImg = document.createElement("img");
-    pngImg.src = pngSrc;
-    pngImagesDiv.appendChild(pngImg);
-  }
-};
+const form = document.getElementById('form');
+const textarea = document.getElementById('textarea');
+const output = document.getElementById('output');
 
-const detectButton = document.getElementById("detect-button");
-detectButton.addEventListener("click", detectPngs);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const html = textarea.value;
+  const regex = /<img[^>]+src="?((.+?)\.(png|jpg|jpeg))"?[^>]*>/g;
+  let match;
+  let images = '';
+  while ((match = regex.exec(html)) !== null) {
+    images += `<img src="${match[1]}">`;
+  }
+  output.innerHTML = images;
+});
